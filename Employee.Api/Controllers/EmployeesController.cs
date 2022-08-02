959c43b7b0efb50bc187ba62a8fda2762b71f973
Dipakshi26 <dipakshisaxena26@gmail.com>
2022-08-02 14:30:46 +0530

    Add project files.

---
+++ Employee.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,79 @@
+﻿using Employee.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Api.Controllers
+{
+    [ApiController]
+    [Route("api/employee")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly EmployeeData _employeeData;
+
+        public EmployeesController(EmployeeData employeeData) => _employeeData = employeeData;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<EmployeeViewModel>> GetEmployees()
+        {
+            return Ok(_employeeData.employeesList);
+        }
+
+        [HttpPost]
+        public ActionResult PostEmployees([FromBody] EmployeeViewModel emp)
+        {
+            var employees = (_employeeData.employeesList);
+
+            employees.Add(new EmployeeViewModel { id = emp.id, FirstName = emp.FirstName, LastName = emp.LastName, MobileNumber = emp.MobileNumber, City = emp.City, Country = emp.Country, AddressLine1 = emp.AddressLine1, AddressLine2 = emp.AddressLine2, Email = emp.Email });
+            return Ok(employees);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEmployee(int empId, EmployeeViewModel employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest("Employee object can't be null");
+            }
+            if (_employeeData.employeesList == null)
+            {
+                return NotFound("Table doesn't exists");
+            }
+            var Update = _employeeData.employeesList.Where(e => e.id == empId).FirstOrDefault();
+            if (Update == null)
+            {
+                return NotFound("Employee with this empId doesn't exists");
+            }
+            _employeeData.employeesList.Remove(Update);
+            Update.id = employee.id;
+            Update.FirstName = employee.FirstName;
+            Update.LastName = employee.LastName;
+            Update.MobileNumber = employee.MobileNumber;
+            Update.City = employee.City;
+            Update.Country = employee.Country;
+            Update.AddressLine1 = employee.AddressLine1;
+            Update.AddressLine2 = employee.AddressLine2;
+            Update.Email = employee.Email;
+
+            _employeeData.employeesList.Add(Update);
+
+            return Ok(_employeeData.employeesList);
+
+        }
+        [HttpDelete]
+        public ActionResult DeleteEmployee(int empId)
+        {
+            if (_employeeData.employeesList == null)
+            {
+                return NotFound("Table doesn't exists");
+            }
+            var employeeToDelete = _employeeData.employeesList.Where(e => e.id == empId).FirstOrDefault();
+            if (employeeToDelete == null)
+            {
+                return NotFound("Employee with this empId doesn't exists");
+            }
+            _employeeData.employeesList.Remove(employeeToDelete);
+            return Ok(_employeeData.employeesList);
+        }
+
+
+    }
+}
---
+++ Employee.Api/EmployeeData.cs
@@ -0,0 +1,56 @@
+﻿using Employee.Api.Models;
+
+namespace Employee.Api
+{
+    public class EmployeeData
+    {
+        public List<EmployeeViewModel> employeesList { get; set; }
+
+        public EmployeeData()
+        {
+            employeesList = new List<EmployeeViewModel>()
+            {
+                 new EmployeeViewModel()
+                {
+                    id = 1,
+                    FirstName = "Dipakshi",
+                    LastName = "Saxena",
+                    MobileNumber = "8115132666",
+                    City = "Lucknow",
+                    Country = "India",
+                    AddressLine1 = "Aliganj",
+                    AddressLine2 = "Aliganj",
+                    Email = "dipakshi@gmail.com",
+                },
+                new EmployeeViewModel()
+                {
+                    id = 2,
+                    FirstName = "Aditya",
+                    LastName = "Singh",
+                    MobileNumber = "562983234",
+                    City = "Lucknow",
+                    Country = "India",
+                    AddressLine1 = "GomtiNagar",
+                    AddressLine2 = "GomtiNagar",
+                    Email = "aditya@gmai.com",
+
+                },
+                new EmployeeViewModel()
+                {
+
+                    id = 3,
+                    FirstName = "Ayush",
+                    LastName = "Gupta",
+                    MobileNumber = "928274923",
+                    City = "Lucknow",
+                    Country = "India",
+                    AddressLine1 = "Bhootnath",
+                    AddressLine2 = "Bhootnath",
+                    Email = "ayush@gmai.com",
+
+                }
+            };
+        }
+    }
+}
+
